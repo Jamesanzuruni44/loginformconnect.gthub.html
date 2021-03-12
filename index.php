66e92660ce8311d200dca0b50203b4aa97365152
jamesanzuruni44 <jamesanzuruniwilondja@gmail.com>
2021-03-13 01:49:35 +0300

    Add new file

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php  
+       $servername = "localhost";  
+       $username = "root";  
+       $password = "";  
+       $conn = mysql_connect ($servername , $username , $password) or die("unable to connect to host");  
+       $sql = mysql_select_db ('test',$conn) or die("unable to connect to database"); 
+?> <?php  
+       $servername = "localhost";  
+       $username = "root";  
+       $password = "";  
+       $conn = mysql_connect ($servername , $username , $password) or die("unable to connect to host");  
+       $sql = mysql_select_db ('test',$conn) or die("unable to connect to database"); 
+?> 
